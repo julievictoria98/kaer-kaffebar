7665f254b05ad280c68411aa3f84defb660d41bd
Julie Radant <jura0002@stud.kea.dk>
2025-05-18 10:49:57 +0200

    small fixes

--- composables/animations/gsap.js
+++ composables/animations/gsap.js
@@ -2,20 +2,19 @@ import { onMounted } from "vue";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-export function useScrollFadeIn(elRef) {
+export function useScrollFadeIn(ref) {
   onMounted(() => {
-    if (!elRef?.value) return;
+    if (!ref?.value) return;
 
     gsap.registerPlugin(ScrollTrigger);
 
-    gsap.from(elRef.value, {
+    gsap.from(ref.value, {
       opacity: 0,
       y: 50,
       duration: 1,
       scrollTrigger: {
-        trigger: elRef.value,
+        trigger: ref.value,
         start: "top 80%",
-        toggleActions: "play none none none",
       },
     });
   });
