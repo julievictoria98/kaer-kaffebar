b90a93a43406710e6b4484c11638443962f4f607
Julie Radant <jura0002@stud.kea.dk>
2025-05-21 16:40:47 +0200

    almost  done with menu

---
+++ pages/menu.vue
@@ -0,0 +1,477 @@
+<template>
+  <article id="menu">
+    <h1 class="font-Yeseva">Menu</h1>
+    <article class="menu">
+      <section>
+        <div>
+          <h2>BRUNCH</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>Kær tallerken</h3>
+              <h3>95,-</h3>
+            </div>
+            <p>
+              Lille morgenomplet med blødkogt øko æg og avkado, yoghurt, frugt
+              smør, syltetøj, øko ost, brød
+            </p>
+            <div>
+              <p class="font-bold mt-2">Tilføj ekstra</p>
+              <p></p>
+            </div>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Kær Brunch</h3>
+              <h3>175,-</h3>
+            </div>
+            <p>
+              Lille morgenomplet med blødkogt øko æg og avkado, yoghurt, frugt
+              smør, syltetøj, øko ost, brLille morgenomplet med blødkogt øko æg
+              og avkado, yoghurt, frugt smør, syltetøj, øko ost, brødødLille
+              morgenomplet med blødkogt øko æg og avkado, yoghurt,
+            </p>
+            <div>
+              <p class="font-bold mt-2">Tilføj ekstra</p>
+              <p></p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>VAFLER</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>Valnødder</h3>
+              <h3>85,-</h3>
+            </div>
+            <p>
+              2 kærlighedsvafler med valnøddecremeost, brombærkompot, valnødder
+              og skovsyre
+            </p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Yoghurt og Bær</h3>
+              <h3>79,-</h3>
+            </div>
+            <p>2 kærlighedsvafler med græsk yoghurt, bær og kokos flager</p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Choko & Banan</h3>
+              <h3>85,-</h3>
+            </div>
+            <p>
+              2 kærlighedsvafler med chokoladesovs, banan, flødeskum, ristede
+              hasselnødder
+            </p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Enkelt Vaffel</h3>
+              <h3>43,-</h3>
+            </div>
+            <p>1 kærlighedsvaffel med sirup</p>
+          </div>
+        </div>
+        <div>
+          <h2>BOWLS</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>Enkelt Vaffel</h3>
+              <h3>43,-</h3>
+            </div>
+            <p>1 kærlighedsvaffel med sirup</p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Enkelt Vaffel</h3>
+              <h3>43,-</h3>
+            </div>
+            <p>1 kærlighedsvaffel med sirup</p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Enkelt Vaffel</h3>
+              <h3>43,-</h3>
+            </div>
+            <p>1 kærlighedsvaffel med sirup</p>
+          </div>
+        </div>
+      </section>
+      <!-- <section>
+        <h2>SMØRREBRØD</h2>
+        <div>
+          <h3>Valnødder 85,-</h3>
+          <p>
+            Lille morgenomplet med blødkogt øko æg og avkado, yoghurt, frugt
+            smør, syltetøj, øko ost, brød
+          </p>
+        </div>
+        <div>
+          <h3>Valnødder 85,-</h3>
+          <p>
+            Lille morgenomplet med blødkogt øko æg og avkado, yoghurt, frugt
+            smør, syltetøj, øko ost, brød
+          </p>
+        </div>
+      </section>
+      <section>
+        <h2>SANDWICH</h2>
+        <div>
+          <h3>Valnødder 85,-</h3>
+          <p>
+            Lille morgenomplet med blødkogt øko æg og avkado, yoghurt, frugt
+            smør, syltetøj, øko ost, brød
+          </p>
+        </div>
+        <div>
+          <h3>Valnødder 85,-</h3>
+          <p>
+            Lille morgenomplet med blødkogt øko æg og avkado, yoghurt, frugt
+            smør, syltetøj, øko ost, brød
+          </p>
+        </div>
+      </section> -->
+    </article>
+    <article class="menu">
+      <section>
+        <div>
+          <h2>SMØRREBRØD</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>Avocadomad 90,-</h3>
+              <h3>90,-</h3>
+            </div>
+            <p>
+              Avocado, hvidløgsspire, råsyltede rødløg, persillepesto<br />
+              <strong>Vælg brød:</strong> Rugbrød / Focaccia<br />
+              <strong>Tilføj:</strong> Smilende æg +20
+            </p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Kartoffelmad</h3>
+              <h3>83,-</h3>
+            </div>
+            <p>
+              Kartoffel, urtemayonnaise, gammel knas, feldsalat, frisk timian,
+              brøndkarse - på rugbrød.
+            </p>
+          </div>
+
+          <div>
+            <div class="flex justify-between">
+              <h3>Æggemad</h3>
+              <h3>83,-</h3>
+            </div>
+            <p>
+              Smilende æg, urtemayo, brøndkarse, dild, feldsalat - på
+              rugbrød.<br />
+              <strong>Tilføj:</strong> Rejer +20
+            </p>
+          </div>
+        </div>
+
+        <div>
+          <h2>GRILLET TOAST</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>Toast</h3>
+              <h3>89 / 90,-</h3>
+            </div>
+            <p>
+              Grillet toast. Ost, skinke, bagt tomat, persillepesto, råsyltede
+              rødløg, på vores hjemmebagte focaccia
+            </p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>Sandwich</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>Tunsandwich</h3>
+              <h3>95,-</h3>
+            </div>
+            <p>
+              Rørt tunsalat, chili flager, hvidløgsspire, salat, pesto, agurk,
+              på vores hjemmebagte focaccia<br />
+              <strong>Tilføj:</strong> Avocado +15
+            </p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Fennikel Salami & Gammel Knas</h3>
+              <h3>95,-</h3>
+            </div>
+            <p>
+              Fennikel salami, gammel knas ost, tomat, råsyltede rødløg, salat,
+              hummus, pesto, på vores hjemmebagte focaccia
+            </p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>Falafelsandwich</h3>
+              <h3>99,-</h3>
+            </div>
+            <p>
+              Falafel, avocado, hummus, salat, agurk, chili, mayo, syltede
+              rødløg, på vores hjemmebagte focaccia
+            </p>
+          </div>
+        </div>
+        <div>
+          <h2>SALAT</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>Falafel Bowl</h3>
+              <h3>109,-</h3>
+            </div>
+            <p>
+              Varm rødbedefalafel, avocado, salat, agurk, persilleolie, sesam og
+              hummus
+            </p>
+          </div>
+        </div>
+      </section>
+    </article>
+    <article class="menu w-full">
+      <section>
+        <div>
+          <h2>BRØD OG PÅLÆG</h2>
+          <div>
+            <div class="flex justify-between">
+              <h3>BMO ØKO OST</h3>
+              <h3>35,-</h3>
+            </div>
+            <p>Hjemmebagt surdejsbolle m. øko ost</p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>BMO GAMMEL KNAS</h3>
+              <h3>45,-</h3>
+            </div>
+            <p>Hjemmebagt surdejsbolle m. Gammel Knas ost</p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>RUGBRØD OG ÆG</h3>
+              <h3>45,-</h3>
+            </div>
+            <p>Smilende øko æg m. smør og ristet rugbrød</p>
+          </div>
+          <div>
+            <div class="flex justify-between">
+              <h3>FYLDT CROISSANT</h3>
+              <h3>45,-</h3>
+            </div>
+            <p>Croissant med ost, skinke og feldsalat. Lunet.</p>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>BAGVÆRK</h2>
+          <div class="flex justify-between">
+            <h3>Croissant</h3>
+            <h3>29,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>Frøsnapper</h3>
+            <h3>29,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>Cookie</h3>
+            <h3>32,-</h3>
+          </div>
+
+          <div class="flex justify-between">
+            <h3>Surdejsbolle</h3>
+            <h3>15,-</h3>
+          </div>
+
+          <div class="flex justify-between">
+            <h3>Øko Kaneltop</h3>
+            <h3>32,-</h3>
+          </div>
+
+          <div class="flex justify-between">
+            <h3>Muffin</h3>
+            <h3>35,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>Cupcake</h3>
+            <h3>42,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>Banankage</h3>
+            <h3>35,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>Mousse</h3>
+            <h3>40/55,-</h3>
+          </div>
+        </div>
+      </section>
+    </article>
+    <article class="menu w-full">
+      <section>
+        <div>
+          <h2>KAFFE MM.</h2>
+          <div class="flex justify-between">
+            <h3>ESPRESSO</h3>
+            <h3>32,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>AMERICANO</h3>
+            <h3>37,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>CORTADO</h3>
+            <h3>40,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>FLAT WHITE</h3>
+            <h3>42,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>CAPPUCCINO</h3>
+            <h3>45,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>CAFÉ LATTE</h3>
+            <h3>47/52,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>VANILJE LATTE</h3>
+            <h3>49,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>TE</h3>
+            <h3>36,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>MATCHA LATTE</h3>
+            <h3>49/54,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>CHAI LATTE</h3>
+            <h3>47/52,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>VARM KAKAO</h3>
+            <h3>45/50,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>VARM HYLDEBLOMST</h3>
+            <h3>42,-</h3>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>ISKAFFE & CHAI</h2>
+          <div class="flex justify-between">
+            <h3>ICED LATTE</h3>
+            <h3>49,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>ICED MATCHA</h3>
+            <h3>49,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>ICED CHAI LATTE</h3>
+            <h3>49,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>ICED AMERICANO</h3>
+            <h3>39,-</h3>
+          </div>
+
+          <h2 class="mt-6">HJEMMELAVET</h2>
+          <div class="flex justify-between">
+            <h3>LIME LEMONADE</h3>
+            <h3>55,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>MYNTE LEMO</h3>
+            <h3>55,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>BROMBÆR LEMO</h3>
+            <h3>55,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>SMOOTHIE</h3>
+            <h3>60,-</h3>
+          </div>
+          <p class="text-sm">Se dagens smoothie</p>
+
+          <h2 class="mt-6">FRISKPRESSET JUICE</h2>
+          <div class="flex justify-between">
+            <h3>FRISKPRESSET JUICE</h3>
+            <h3>59,-</h3>
+          </div>
+          <p class="text-sm">Friskpresset æppelsinjuice</p>
+          <p class="text-sm">Tilføj: Ingefær shot +10</p>
+
+          <div class="flex justify-between mt-4">
+            <h3>INGEFÆR SHOT</h3>
+            <h3>22,-</h3>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div>
+          <h2>LÆSKEDRIKKE</h2>
+          <div class="flex justify-between">
+            <h3>HYLDEBLOMST</h3>
+            <h3>35,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>MATÉ MATÉ</h3>
+            <h3>45,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>IPA ALKOHOLFRI</h3>
+            <h3>39,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>COCIO</h3>
+            <h3>35,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>DANSK VAND</h3>
+            <h3>35,-</h3>
+          </div>
+          <div class="flex justify-between">
+            <h3>BIE'S ØKO</h3>
+            <h3>35,-</h3>
+          </div>
+          <p class="text-sm">rabarber / solbær</p>
+          <p class="text-sm">æblemost / æble & ingefær</p>
+
+          <h2 class="mt-6">SÆSON</h2>
+          <div class="flex justify-between">
+            <h3>SPICED LATTE</h3>
+            <h3>49,-</h3>
+          </div>
+          <p class="text-sm">Latte med hjemmelavet kryddersirup og kanel</p>
+
+          <div class="flex justify-between mt-4">
+            <h3>SPICED ICED LATTE</h3>
+            <h3>52,-</h3>
+          </div>
+          <p class="text-sm">
+            Iskaffe med hjemmelavet kryddersirup, kanel og flødeskum
+          </p>
+        </div>
+      </section>
+    </article>
+  </article>
+</template>
+
+<style></style>
