6e61848e551eff7dd55c620b5247a567bcd7ad5c
Julie Radant <jura0002@stud.kea.dk>
2025-05-26 18:11:59 +0200

    rettet link

--- components/index/Hero.vue
+++ components/index/Hero.vue
@@ -5,11 +5,9 @@
     <div id="hero" class="h-screen flex flex-col justify-center col-span-full">
       <h1 class="display">KÆR KAFFEBAR</h1>
       <div class="flex flex-col gap-4 sm:flex-row sm:gap-12">
-        <UiSecondaryButton text="MENUKORT" href="/contact"></UiSecondaryButton>
-        <UiSecondaryButton text="BOOK BORD" href="/contact"></UiSecondaryButton>
+        <UiSecondaryButton text="MENUKORT" href="/menu"></UiSecondaryButton>
+        <UiSecondaryButton text="BOOK BORD" href="/kontakt"></UiSecondaryButton>
       </div>
-
-      <PrimaryButton text="Click me" href="/contact"></PrimaryButton>
     </div>
   </article>
 </template>
